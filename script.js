9f7d583de4c071a980be6bfb8b06cb512de0462c
Kaushik20-02 <111055319+Kaushik20-02@users.noreply.github.com>
2025-11-23 20:06:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const second = 1000,
   minute = second * 60,
   hour = minute * 60,
   day = hour * 24;
-let countDown = new Date('Nov 30, 2025 00:00:00').getTime(),
+let countDown = new Date('Nov 3, 2025 00:00:00').getTime(),
   x = setInterval(function () {
     let now = new Date().getTime(),
       distance = countDown - now;
@@ -490,5 +490,6 @@ function confetti() {
 
   if (!onlyOnKonami) poof();
 };
+
 
 
